f08f64e8e546bb8cb2425f6b170dbaafdd05adc8
BluePenguin42069 <95193828+BluePenguin42069@users.noreply.github.com>
2021-12-01 12:45:10 -0500

    Add files via upload

---
+++ TASK1_Revised2_Final_Version/TASK1_Revised2_Final_Version.ino
@@ -0,0 +1,70 @@
+#include <IRremote.h>
+#include<Servo.h>
+
+Servo Myservo;
+int pos1=150;
+int pos2=105;
+int pos3=195;
+int speedPin=5;
+int dir1=4;
+int dir2=3;
+int mSpeed=255;
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+unsigned long key_value = 0;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(speedPin, OUTPUT);
+pinMode(dir1, OUTPUT);
+pinMode(dir2, OUTPUT);
+Myservo.attach(6);  
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+
+void loop(){
+  if (irrecv.decode(&results)){
+
+switch(results.value){
+
+case 0xFF629D:
+Serial.println("up");
+digitalWrite(dir1,HIGH);
+analogWrite(speedPin, mSpeed);
+Myservo.write(pos1);
+delay(800);
+break;  
+
+      
+case 0xFFC23D:
+Serial.println("right");
+digitalWrite(dir1,HIGH);
+analogWrite(speedPin, mSpeed);
+Myservo.write(pos3); 
+delay(2000);
+break;
+
+      
+case 0xFF22DD:
+Serial.println("left");
+digitalWrite(dir1,HIGH);
+analogWrite(speedPin, mSpeed);
+Myservo.write(pos2); 
+delay(2000);
+break;
+
+default:       
+//case 0XFF6897:
+Serial.println("stop");
+digitalWrite(dir1,LOW);
+analogWrite(speedPin, mSpeed);
+Myservo.write(pos1); 
+break;
+}
+delay(100);
+irrecv.resume();
+  }
+}
